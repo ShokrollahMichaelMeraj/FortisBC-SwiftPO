0f95ef5276b8cc16f8e38ba6c1d57ebfcf161cf2
Andy Do <dohoaan2003@gmail.com>
2023-07-02 14:59:20 -0700

    testing

---
+++ fortisbc/src/main/java/com/example/fortisbc/FortisbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.fortisbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortisbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortisbcApplication.class, args);
+	}
+
+}
