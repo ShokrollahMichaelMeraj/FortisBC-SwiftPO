e47ac6a17531313794b7ba014f158f2b414cde54
NormanCao2702 <norman.cao2702@gmail.com>
2023-07-01 18:09:28 -0700

    initial commit

---
+++ demo/src/main/java/cmpt276/project/demo/ProjectApplication.java
@@ -0,0 +1,13 @@
+package cmpt276.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/cmpt276/project/demo/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cmpt276.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
