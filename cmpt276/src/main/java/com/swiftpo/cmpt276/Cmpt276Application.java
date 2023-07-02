84c8f473ea681c836f5d1583a2cfa3082c0d5690
ShokrollahMichaelMeraj <merajmichael@gmail.com>
2023-07-01 17:52:57 -0700

    adding maven start spring initializer package and changing the location of the readme.md file

---
+++ cmpt276/src/main/java/com/swiftpo/cmpt276/Cmpt276Application.java
@@ -0,0 +1,13 @@
+package com.swiftpo.cmpt276;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmpt276Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmpt276Application.class, args);
+	}
+
+}
---
+++ cmpt276/src/test/java/com/swiftpo/cmpt276/Cmpt276ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftpo.cmpt276;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmpt276ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
